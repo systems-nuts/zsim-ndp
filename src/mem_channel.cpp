472635cc659e9c07077e41ecdb5681b8ab4af655
Mingyu Gao <mgao12@stanford.edu>
2016-04-22 15:04:40 -0700

    [mem_channel] Implement the event-based simulation scheme.

---
+++ src/mem_channel.cpp
@@ -0,0 +1,203 @@
+#include "mem_channel.h"
+#include "contention_sim.h"
+#include "timing_event.h"
+#include "zsim.h"
+
+//#define DEBUG(args...) info(args)
+#define DEBUG(args...)
+
+// Weave phase event for one memory access request.
+class MemChannelAccEvent : public TimingEvent {
+    private:
+        MemChannel* mem;
+        Address addr;
+        bool write;
+
+    public:
+        MemChannelAccEvent(MemChannel* _mem, bool _isWrite, Address _addr, int32_t domain,
+                uint32_t preDelay, uint32_t postDelay)
+            : TimingEvent(preDelay, postDelay, domain), mem(_mem), addr(_addr), write(_isWrite) {}
+
+        Address getAddr() const { return addr; }
+        bool isWrite() const { return write; }
+
+        void simulate(uint64_t startCycle) {
+            mem->acceptAccEvent(this, startCycle);
+        }
+};
+
+// Weave phase event for memory system tick.
+class MemChannelTickEvent : public TimingEvent, public GlobAlloc {
+    private:
+        MemChannel* const mem;
+        enum State { IDLE, QUEUED, RUNNING, ANNULLED } state;
+
+    public:
+        MemChannelTickEvent(MemChannel* _mem, int32_t domain)
+            : TimingEvent(0, 0, domain), mem(_mem)
+        {
+            setMinStartCycle(0);
+            setRunning();
+            state = IDLE;
+            hold();
+        }
+
+        void parentDone(uint64_t startCycle) {
+            panic("This is queued directly");
+        }
+
+        void simulate(uint64_t startCycle) {
+            assert(state == QUEUED || state == ANNULLED);
+            if (state == QUEUED) {
+                state = RUNNING;
+                uint64_t nextCycle = mem->tick(startCycle);
+                if (nextCycle) {
+                    assert(nextCycle >= startCycle);
+                    requeue(nextCycle);
+                    state = QUEUED;
+                    return;
+                }
+            }
+            // Recycle this event.
+            state = IDLE;
+            hold();
+            mem->recycleTickEvent(this);
+        }
+
+        void enqueue(uint64_t cycle) {
+            assert(state == IDLE);
+            state = QUEUED;
+            // Enqueue happens in weave phase
+            requeue(cycle);
+        }
+
+        void annul() {
+            assert(state == QUEUED);
+            state = ANNULLED;
+        }
+
+        // Use glob mem
+        using GlobAlloc::operator new;
+        using GlobAlloc::operator delete;
+};
+
+void MemChannel::initStats(AggregateStat* parentStat) {
+}
+
+uint64_t MemChannel::access(MemReq& req) {
+    switch (req.type) {
+        case PUTS:
+        case PUTX:
+            *req.state = I;
+            break;
+        case GETS:
+            *req.state = req.is(MemReq::NOEXCL) ? S : E;
+            break;
+        case GETX:
+            *req.state = M;
+            break;
+        default: panic("!?");
+    }
+
+    if (req.type == PUTS) {
+        return req.cycle; // zero latency
+    } else {
+        bool isWrite = (req.type == PUTX);
+        uint64_t respCycle = req.cycle + (isWrite ? minWrDelay : minRdDelay);
+        if (zinfo->eventRecorders[req.srcId]) {
+            MemChannelAccEvent* memEv = new (zinfo->eventRecorders[req.srcId])
+                MemChannelAccEvent(this, isWrite, req.lineAddr, domain,
+                        isWrite ? preWrDelay : preRdDelay,
+                        isWrite ? postWrDelay : postRdDelay);
+            memEv->setMinStartCycle(req.cycle);
+            TimingRecord tr = {req.lineAddr, req.cycle, respCycle, req.type, memEv, memEv};
+            zinfo->eventRecorders[req.srcId]->pushRecord(tr);
+        }
+        return respCycle;
+    }
+}
+
+void MemChannel::acceptAccEvent(MemChannelAccEvent* ev, uint64_t sysCycle) {
+    DEBUG("Accept AccEvent to 0x%lx at sysCycle %lu", ev->getAddr(), sysCycle);
+
+    // Hold the access event for further scheduling/issuing. Release when responding.
+    ev->hold();
+
+    // Request queue overflow check.
+    if (checkOverflow(ev->isWrite())) {
+        overflowQueue.emplace_back(ev, sysCycle);
+        return;
+    }
+
+    // Schedule.
+    uint64_t memCycle = sysToMemCycle(sysCycle);
+    DEBUG("Schedule new request (0x%lx,%c) at %lu", ev->getAddr(), ev->isWrite()?'w':'r', memCycle);
+    uint64_t estTickCycle = schedule(ev, sysCycle, memCycle);
+
+    // Adjust the tick cycle since the new request may be able to issued earlier.
+    if (estTickCycle < tickCycle) {
+        tickCycle = estTickCycle;
+
+        // Annul the current tick event.
+        if (tickEvent) tickEvent->annul();
+        // Pick and set the new tick event.
+        if (freeTickEvent != nullptr) {
+            tickEvent = freeTickEvent;
+            freeTickEvent = nullptr;
+        } else {
+            tickEvent = new MemChannelTickEvent(this, domain);
+        }
+        uint64_t enqSysCycle = matchingSysCycle(tickCycle);
+        assert(enqSysCycle >= sysCycle);
+        tickEvent->enqueue(enqSysCycle);
+        DEBUG("Tick cycle shifted to %lu by new request", tickCycle);
+    }
+}
+
+void MemChannel::respondAccEvent(MemChannelAccEvent* ev, uint64_t sysCycle) {
+    bool isWrite = ev->isWrite();
+    ev->release();
+    ev->done(sysCycle - (isWrite ? minWrDelay : minRdDelay));
+}
+
+uint64_t MemChannel::tick(uint64_t sysCycle) {
+    uint64_t memCycle = sysToMemCycle(sysCycle);
+    assert_msg(memCycle == tickCycle, "Tick at wrong time %lu (should be %lu)", memCycle, tickCycle);
+
+    // Try issue and get the next tick cycle.
+    uint64_t nextTickCycle = issue(memCycle);
+    assert(nextTickCycle > memCycle);
+
+    // Try to schedule an overflow request.
+    if (!overflowQueue.empty()) {
+        auto& oe = overflowQueue.front();
+        MemChannelAccEvent* ev = oe.first;
+        uint64_t startCycle = oe.second;
+        if (!checkOverflow(ev->isWrite())) {
+            DEBUG("Schedule overflow request (0x%lu,%c) at %lu", ev->getAddr(), ev->isWrite()?'w':'r', memCycle);
+            uint64_t estTickCycle = schedule(ev, startCycle, memCycle);
+            overflowQueue.pop_front();
+            if (estTickCycle < nextTickCycle) {
+                // The new scheduled request could make tick cycle earlier.
+                nextTickCycle = estTickCycle;
+                DEBUG("Tick event shifted to %lu by overflow request", nextTickCycle);
+            }
+        }
+    }
+
+    // Update tick cycle.
+    tickCycle = nextTickCycle;
+    if (tickCycle == -1uL) {
+        tickEvent = nullptr;
+        return 0; // 0 means no more event.
+    }
+    return std::max(sysCycle, matchingSysCycle(tickCycle)); // make sure no go back in time.
+}
+
+void MemChannel::recycleTickEvent(MemChannelTickEvent* tev) {
+    assert(tev != tickEvent);
+    if (freeTickEvent == nullptr) freeTickEvent = tev;
+    else delete tev;
+}
+
+
---
+++ src/mem_channel.h
@@ -0,0 +1,109 @@
+#ifndef MEM_CHANNEL_H_
+#define MEM_CHANNEL_H_
+
+#include <deque>
+#include "finite_queue.h"
+#include "g_std/g_string.h"
+#include "intrusive_list.h"
+#include "memory_hierarchy.h"
+#include "stats.h"
+
+class MemChannelAccEvent;
+class MemChannelTickEvent;
+
+class MemChannel : public MemObject {
+    private:
+        // Access request record. Linked in a linked list based on the priority.
+        struct AccReq : InListNode<AccReq> {
+            //TODO
+        };
+
+    public:
+        MemChannel(/*TODO*/);
+
+        const char* getName() { return name.c_str(); }
+
+        void initStats(AggregateStat* parentStat);
+
+        /* Bound phase. */
+        uint64_t access(MemReq& req);
+
+        /* Weave phase. */
+        void acceptAccEvent(MemChannelAccEvent* ev, uint64_t sysCycle);
+        void respondAccEvent(MemChannelAccEvent* ev, uint64_t sysCycle);
+
+        uint64_t tick(uint64_t sysCycle);
+        void recycleTickEvent(MemChannelTickEvent* tev);
+
+    private:
+        const g_string name;
+        const uint32_t domain;
+
+        /* Controller timing. */
+
+        // Bound phase delays, in sys cycles.
+        uint32_t minRdDelay, minWrDelay, preRdDelay, preWrDelay, postRdDelay, postWrDelay;
+
+        uint64_t sysFreqKHz;
+        uint64_t memFreqKHz;
+
+        // sys <-> mem cycle translation.
+        // We receive and return system cycles, but all internal logic is in memory cycles.
+        // The +1 ensures that we never go back in time when translating, i.e., sync across clock
+        // domain makes event be handled in the next cycle.
+        inline uint64_t sysToMemCycle(uint64_t sysCycle) const {
+            return sysCycle * memFreqKHz / sysFreqKHz + 1;
+        }
+        inline uint64_t memToSysCycle(uint64_t memCycle) const {
+            return memCycle * sysFreqKHz / memFreqKHz + 1;
+        }
+        // Ensure sysToMemCycle(matchingSysCycle(memCycle)) == memCycle.
+        inline uint64_t matchingSysCycle(uint64_t memCycle) const {
+            // mC <= sC*mF/sF+1 < mC+1  ==>  (mC-1)*sF/mF <= sC < mC*sF/mF
+            // So sC is the max int that less than mC*sF/mF (no equal), i.e., (mC*sF-1/2)/mF.
+            return (memCycle * sysFreqKHz * 2 - 1) / 2 / memFreqKHz;
+        }
+
+        /* Scheduling. */
+
+        // Tick cycle for the next event, in mem cycles.
+        uint64_t tickCycle;
+        // Tick event.
+        MemChannelTickEvent* tickEvent;
+        // Keep one free tick event for fast alloc/dealloc.
+        MemChannelTickEvent* freeTickEvent;
+
+        // Overflow request queue, not considered when scheduling.
+        // Record AccEvent and startCycle.
+        std::deque<std::pair<MemChannelAccEvent*, uint64_t>> overflowQueue;
+
+        // Whether to use separate queues for read and write requests.
+        const bool sepWrites;
+        // Read/write request queue.
+        const uint32_t reqQueueDepth;
+        typedef FiniteQueue<AccReq> ReqQueue;
+        ReqQueue rdReqQueue, wrReqQueue;
+
+        inline ReqQueue& getReqQueue(bool isWrite) {
+            return (sepWrites && isWrite) ? wrReqQueue : rdReqQueue;
+        }
+        inline const ReqQueue& getReqQueue(bool isWrite) const {
+            return (sepWrites && isWrite) ? wrReqQueue : rdReqQueue;
+        }
+        inline bool checkOverflow(bool isWrite) const { return getReqQueue(isWrite).full(); }
+
+        /* Stats. */
+
+
+    private:
+        // Schedule event \c ev started at \c startCycle, at current time \c memCycle.
+        // Return the estimated tick time for this access request in mem cycle.
+        uint64_t schedule(MemChannelAccEvent* ev, uint64_t startCycle, uint64_t memCycle);
+
+        // Try to issue a request whose tick time is no later than current time \c memCycle.
+        // Return the next tick time in mem cycle.
+        uint64_t issue(uint64_t memCycle);
+
+};
+
+#endif  // MEM_CHANNEL_H_
