25d4ad43e15f520aee9aa9a48b6e15685b84217b
Mingyu Gao <gaomy@tsinghua.edu.cn>
2019-05-03 15:52:48 +0800

    [build/virt] Fix syscall header path.

--- misc/list_syscalls.py
+++ misc/list_syscalls.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python
 # Produces a list of syscalls in the current system
 import os, re
-syscallCmd = "gcc -E -dD /usr/include/asm/unistd.h | grep __NR"
+filePattern = "/usr/include/{}/unistd.h"
+fileName = filePattern.format("asm")
+if not os.path.exists(fileName):
+    fileName = filePattern.format("asm-generic")
+    assert os.path.exists(fileName)
+syscallCmd = "gcc -E -dD {} | grep __NR".format(fileName)
 syscallDefs = os.popen(syscallCmd).read()
 sysList = [(int(numStr), name) for (name, numStr) in re.findall("#define __NR_(.*?) (\d+)", syscallDefs)]
 denseList = ["INVALID"]*(max([num for (num, name) in sysList]) + 1)
