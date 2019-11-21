9785333b4a11e1f1385dad28ff454fe5c3f6518c
Mingyu Gao <gaomy@tsinghua.edu.cn>
2019-11-21 14:58:04 +0800

    [build/virt] Fix syscall header generation again.

--- misc/list_syscalls.py
+++ misc/list_syscalls.py
@@ -1,12 +1,7 @@
 #!/usr/bin/python
 # Produces a list of syscalls in the current system
 import os, re
-filePattern = "/usr/include/{}/unistd.h"
-fileName = filePattern.format("asm")
-if not os.path.exists(fileName):
-    fileName = filePattern.format("asm-generic")
-    assert os.path.exists(fileName)
-syscallCmd = "gcc -E -dD {} | grep __NR".format(fileName)
+syscallCmd = "printf \"#include <sys/syscall.h>\" | gcc -E -dD - | grep __NR"
 syscallDefs = os.popen(syscallCmd).read()
 sysList = [(int(numStr), name) for (name, numStr) in re.findall("#define __NR_(.*?) (\d+)", syscallDefs)]
 denseList = ["INVALID"]*(max([num for (num, name) in sysList]) + 1)
